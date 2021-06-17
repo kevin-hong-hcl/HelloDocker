565f761ee0d4a4ec4f8c52118c2ba981e7d34ebf
Kevin Hong <kevin.hong@hcl.com>
2021-06-17 12:32:35 -0400

    first commit

---
+++ src/main/java/com/khong/MyDockerHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.khong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDockerHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDockerHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khong/MyDockerHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDockerHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
